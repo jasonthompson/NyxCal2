3d6375d2986be22289eacc2556f910da1786163e
Jason Thompson <jason@jthompson.ca>
2014-02-04 05:47:04 -0500

    Remove commented out code

--- src/ui/app.js
+++ src/ui/app.js
@@ -6,21 +6,12 @@ module.exports = (function() {
   var Calendar = require('../models/calendar');
   var root = $('#nyxcal-app');
 
-  // $('#left-nav').click(function(){
-  //   return $.route($(this).attr('href'));
-  // });
-
-  // $('#right-nav').click(function(){
-  //   return $.route($(this).attr('href'));
-  // });
-
   $.route(function(hash) {
     var month = hash.slice(7);
     var year = hash.slice(2,6);
     var CalendarView = require('./calendar-view');
     var calendar = new Calendar(month, year);
     var currentMonthView = new CalendarView(calendar, root);
-
 
     currentMonthView.render();
   });
